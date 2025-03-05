dc915f615d9dae8d12e439b961a87ecbe98ea3d9
raul <raulssorban@gmail.com>
2025-03-05 01:15:44 +0100

    Patrette's idea++

--- Tools/Build/linux/update.sh
+++ Tools/Build/linux/update.sh
@@ -14,7 +14,7 @@ TARGET=${1:-release}
 
 for OS in windows linux; do
 	# Download rust binary libs
-	"${ROOT}/Tools/DepotDownloader/DepotDownloader/bin/Release/net8.0/DepotDownloader" \
+	dotnet "${ROOT}/Tools/DepotDownloader/DepotDownloader/bin/Release/net8.0/DepotDownloader.dll" \
 		-os ${OS} -validate -app 258550 -branch ${TARGET} -filelist \
 		"${ROOT}/Tools/Helpers/258550_refs.txt" -dir "${ROOT}/Rust/${OS}"
 done
@@ -24,7 +24,7 @@ dotnet clean   "${ROOT}/Carbon.Core" --configuration Debug
 dotnet build   "${ROOT}/Carbon.Core" --configuration Debug
 
 for OS in windows linux; do
-	"${ROOT}/Carbon.Core/Carbon.Tools/Carbon.Publicizer/bin/x64/Debug/net8.0/Carbon.Publicizer" \
+	dotnet "${ROOT}/Carbon.Core/Carbon.Tools/Carbon.Publicizer/bin/x64/Debug/net8.0/Carbon.Publicizer.dll" \
 		-input "${ROOT}/Rust/${OS}/RustDedicated_Data/Managed" -carbon.rustrootdir "${ROOT}/Rust/${OS}" -carbon.logdir "${ROOT}/Rust/${OS}"
 done
 
@@ -32,14 +32,14 @@ dotnet restore "${ROOT}/Carbon.Core"
 dotnet clean   "${ROOT}/Carbon.Core" --configuration Debug
 dotnet build   "${ROOT}/Carbon.Core" --configuration Debug
 
-"${ROOT}/Carbon.Core/Carbon.Tools/Carbon.Generator/bin/x64/Debug/net8.0/Carbon.Generator" \
+dotnet "${ROOT}/Carbon.Core/Carbon.Tools/Carbon.Generator/bin/x64/Debug/net8.0/Carbon.Generator.dll" \
 	--plugininput "${ROOT}/Carbon.Core/Carbon.Components/Carbon.Common/src/Carbon/Core" \
 	--pluginoutput "${ROOT}/Carbon.Core/Carbon.Components/Carbon.Common/src/Carbon/Core/Core.Plugin-Generated.cs"
 
 for MODULE in "${ROOT}/Carbon.Core/Carbon.Components/Carbon.Common/src/Carbon/Modules/"*; do
 	if [ -d "${MODULE}" ] 
 	then
-	"${ROOT}/Carbon.Core/Carbon.Tools/Carbon.Generator/bin/x64/Debug/net8.0/Carbon.Generator" \
+	dotnet "${ROOT}/Carbon.Core/Carbon.Tools/Carbon.Generator/bin/x64/Debug/net8.0/Carbon.Generator.dll" \
 		--plugininput "${MODULE}" \
 		--pluginoutput "${MODULE}/$(basename "${MODULE}")-Generated.cs" \
 		--pluginname "$(basename "${MODULE}")" \
@@ -51,7 +51,7 @@ done
 for MODULE in "${ROOT}/Carbon.Core/Carbon.Components/Carbon.Modules/src/"*; do
 	if [ -d "${MODULE}" ] 
 	then
-	"${ROOT}/Carbon.Core/Carbon.Tools/Carbon.Generator/bin/x64/Debug/net8.0/Carbon.Generator" \
+	dotnet "${ROOT}/Carbon.Core/Carbon.Tools/Carbon.Generator/bin/x64/Debug/net8.0/Carbon.Generator.dll" \
 		--plugininput "${MODULE}" \
 		--pluginoutput "${MODULE}/$(basename "${MODULE}")-Generated.cs" \
 		--pluginname "$(basename "${MODULE}")" \
